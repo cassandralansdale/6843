10aeea70b6e2ceb266025ef23058fea589452191
cassandralansdale <cdl9860@nyu.edu>
2021-09-07 16:09:46 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 
 def welcome_assignment_answers(question):
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-        answer = "mTLS"
+        answer = "No"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
