3d0eb58bf2dcd888d1a5042d0ad4be190fe6964f
cassandralansdale <cdl9860@nyu.edu>
2021-09-07 15:56:10 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -19,8 +19,3 @@ def welcome_assignment_answers(question):
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 3
     return(answer)
-
-if __name__ == "__main__":
-    # use this space to debug and verify that the program works
-    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
-    print(welcome_assignment_answers(debug_question))
