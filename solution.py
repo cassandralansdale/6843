17748c3e298fcee3290a7578b936631713acc383
cassandralansdale <90283641+cassandralansdale@users.noreply.github.com>
2021-12-02 15:00:25 -0500

    .

--- solution.py
+++ solution.py
@@ -139,21 +139,23 @@ def get_route(hostname):
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
-                    print('%d, %.0fms, %s, %s' %(ttl,(timeReceived - t)*1000, addr[0], name))
+                    tracelist1 = ('%d, %.0fms, %s, %s' %(ttl,(timeReceived - t)*1000, addr[0], name))
+                    tracelist2.extend(tracelist1)
                     #You should add your responses to your lists here
                     #Fill in end
                 elif types == 3:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    print('%d, %.0fms, %s, %s' %(ttl,(timeReceived - t)*1000, addr[0], name))
-                    
+                    tracelist1 = ('%d, %.0fms, %s, %s' %(ttl,(timeReceived - t)*1000, addr[0], name))
+                    tracelist2.extend(tracelist1)
                     #Fill in start
                     #You should add your responses to your lists here 
                     #Fill in end
                 elif types == 0:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    print('%d, %.0fms, %s, %s' %(ttl,(timeReceived - t)*1000, addr[0], name))
+                    tracelist1 = ('%d, %.0fms, %s, %s' %(ttl,(timeReceived - t)*1000, addr[0], name))
+                    tracelist2.extend(tracelist1)
                     #Fill in start
                     #You should add your responses to your lists here and return your list if your destination IP is met
                     #Fill in end
