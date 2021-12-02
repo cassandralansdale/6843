99d9802ecb8984d10ada0027453410b213f54bd7
cassandralansdale <90283641+cassandralansdale@users.noreply.github.com>
2021-12-02 11:04:07 -0500

    .

--- solution.py
+++ solution.py
@@ -16,15 +16,15 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     #clientSocket.send(tls.encode())
     # Fill in end
     recv = clientSocket.recv(1024).decode()
-    print(recv)
-    if recv[:3] != '220':
-        print('220 reply not received from server.')
+    #print(recv)
+    #if recv[:3] != '220':
+        #print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    print(recv1)
+    #print(recv1)
     
 
     # Send MAIL FROM command and print server response.
@@ -32,7 +32,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     mailFrom = "MAIL FROM: <mail@mail.com> \r\n"
     clientSocket.send(mailFrom.encode())
     recv2 = clientSocket.recv(2024).decode()
-    print(recv2)
+    #print(recv2)
     
 
     # Send RCPT TO command and print server response.
@@ -40,7 +40,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     rcptTo = "RCPT TO: <mail@mail.com> \r\n"
     clientSocket.send(rcptTo.encode())
     recv3 = clientSocket.recv(1024).decode()
-    print(recv3)
+    #print(recv3)
     
     # Fill in end
 
@@ -49,7 +49,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     data = "DATA <XXX> \r\n"
     clientSocket.send(data.encode())
     recv4 = clientSocket.recv(1024).decode()
-    print(recv4)
+    #print(recv4)
     
     # Fill in end
 
@@ -71,7 +71,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     #quit = "Quit \r\n"
     clientSocket.send("QUIT \r\n".encode())
     recv5 = clientSocket.recv(1024).decode()
-    print(recv5)
+    #print(recv5)
     clientSocket.close()
     # Fill in end
 
